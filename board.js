6ca975ed15e857e200e3e0010e18d03f0a70b669
Nicollette <nicolletteml@gmail.com>
2014-02-24 11:25:16 -0800

    Added board class and moved board logic from game class

--- board.js
+++ board.js
@@ -1,3 +1,114 @@
 (function (root) {
   var TTT = root.TTT = (root.TTT || {});
+  
+  var Board = TTT.Board = function () {
+    this.tiles = this.makeBoard();
+    this.marks = ["orange", "blue"];
+  }
+  
+  // Board.marks = ["orange", "blue"];
+  
+  Board.prototype.makeBoard = function () {
+    return _.times(9, function (i) {
+      return null;
+    });
+  };
+  
+  Board.prototype.diagonalWinner = function () {
+    var board = this;
+
+    var diagonalPositions1 = [0, 4, 8];
+    var diagonalPositions2 = [2, 4, 6];
+
+    var winner = null;
+    _(this.marks).each(function (mark) {
+			function didWinDiagonal (diagonalPositions) {
+        return _.every(diagonalPositions, function (pos) {
+          return board.tiles[pos] === mark;
+      	});
+    	}
+
+      var won = _.any(
+        [diagonalPositions1, diagonalPositions2],
+        didWinDiagonal
+      );
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+
+  Board.prototype.verticalWinner = function () {
+    var board = this;
+		var verticalPositions1 = [0, 3, 6];
+		var verticalPositions2 = [1, 4, 7];
+		var verticalPositions3 = [2, 5, 8];
+
+    var winner = null;
+    _(this.marks).each(function (mark) {
+      function didWinVertical (verticalPositions) {
+        return _.every(verticalPositions, function (pos) {
+          return board.tiles[pos] === mark;
+				});
+			}
+
+      var won = _.any(
+        [verticalPositions1, verticalPositions2, verticalPositions3],
+        didWinVertical
+      );
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+
+  Board.prototype.horizontalWinner = function () {
+    var board = this;
+		var horizontalPositions1 = [0, 1, 2];
+		var horizontalPositions2 = [3, 4, 5];
+		var horizontalPositions3 = [6, 7, 8];
+
+    var winner = null;
+    _(this.marks).each(function (mark) {
+      function didWinVertical (horizontalPositions) {
+        return _.every(horizontalPositions, function (pos) {
+          return board.tiles[pos] === mark;
+				});
+			}
+
+      var won = _.any(
+        [horizontalPositions1, horizontalPositions2, horizontalPositions3],
+        didWinVertical
+      );
+
+      if (won) {
+        winner = mark;
+      }
+    });
+
+    return winner;
+  };
+  
+  Board.prototype.winner = function () {
+    return (
+      this.diagonalWinner() || this.horizontalWinner() || this.verticalWinner()
+    );
+  };
+  
+  Board.prototype.isBoardFull = function () {
+    var boardIsFull = true;
+    this.tiles.forEach(function (tile) {
+      if (!tile) {
+        boardIsFull = false;
+      }
+    });
+    
+    return boardIsFull;
+  };
 })(this);
--- ttt.js
+++ ttt.js
@@ -2,27 +2,27 @@
 	var TTT = root.TTT = (root.TTT || {});
 
   var Game = TTT.Game = function () {
-    this.player = Game.marks[0];
-    this.board = this.makeBoard();
+    this.board = new TTT.Board;
+    this.player = this.board.marks[0];
   };
-  Game.marks = ["orange", "blue"];
+  // Game.marks = ["orange", "blue"];
 
-  Game.prototype.makeBoard = function () {
-    return _.times(9, function (i) {
-      return null;
-    });
-  };
+  // Game.prototype.makeBoard = function () {
+  //   return _.times(9, function (i) {
+  //     return null;
+  //   });
+  // };
 
 	Game.prototype.placeMark = function(tile_id) {
-		this.board[parseInt(tile_id)] = this.player;
+		this.board.tiles[parseInt(tile_id)] = this.player;
 	};
 
   Game.prototype.switchPlayer = function () {
-    if (this.player === Game.marks[0]) {
-      this.player = Game.marks[1];
+    if (this.player === this.board.marks[0]) {
+      this.player = this.board.marks[1];
       $("#curr-player").text("blue's turn")
     } else {
-      this.player = Game.marks[0];
+      this.player = this.board.marks[0];
       $("#curr-player").text("orange's turn")
     }
   };
@@ -34,31 +34,31 @@
 	Game.prototype.handleTClick = function(event) {
     event.preventDefault();
     
-		this.changeTileColor(event.currentTarget)
+		this.changeTileColor(event.currentTarget);
 		$(event.currentTarget).off("click");
 		this.placeMark(event.currentTarget.id);
 		this.switchPlayer();
 
-		if (this.winner()) {
-			alert(this.winner() + " won!");
+		if (this.board.winner()) {
+			alert(this.board.winner() + " won!");
 			this.gameOverClicks();
-		} else if (this.isBoardFull()) {
+		} else if (this.board.isBoardFull()) {
       alert("Game over, no more moves left.");
       this.gameOverClicks();
     }
 
 	};
   
-  Game.prototype.isBoardFull = function () {
-    var boardIsFull = true;
-    this.board.forEach(function (tile) {
-      if (!tile) {
-        boardIsFull = false;
-      }
-    });
-    
-    return boardIsFull;
-  };
+  // Game.prototype.isBoardFull = function () {
+  //   var boardIsFull = true;
+  //   this.board.forEach(function (tile) {
+  //     if (!tile) {
+  //       boardIsFull = false;
+  //     }
+  //   });
+  //   
+  //   return boardIsFull;
+  // };
   
 	Game.prototype.gameOverClicks = function() {
 		$('.tile').off("click");
@@ -73,92 +73,92 @@
 		}
 	};
 
-  Game.prototype.diagonalWinner = function () {
-    var game = this;
-
-    var diagonalPositions1 = [0, 4, 8];
-    var diagonalPositions2 = [2, 4, 6];
-
-    var winner = null;
-    _(Game.marks).each(function (mark) {
-			function didWinDiagonal (diagonalPositions) {
-        return _.every(diagonalPositions, function (pos) {
-          return game.board[pos] === mark;
-      	});
-    	}
-
-      var won = _.any(
-        [diagonalPositions1, diagonalPositions2],
-        didWinDiagonal
-      );
-
-      if (won) {
-        winner = mark;
-      }
-    });
-
-    return winner;
-  };
-
-  Game.prototype.verticalWinner = function () {
-    var game = this;
-		var verticalPositions1 = [0, 3, 6];
-		var verticalPositions2 = [1, 4, 7];
-		var verticalPositions3 = [2, 5, 8];
-
-    var winner = null;
-    _(Game.marks).each(function (mark) {
-      function didWinVertical (verticalPositions) {
-        return _.every(verticalPositions, function (pos) {
-          return game.board[pos] === mark;
-				});
-			}
-
-      var won = _.any(
-        [verticalPositions1, verticalPositions2, verticalPositions3],
-        didWinVertical
-      );
-
-      if (won) {
-        winner = mark;
-      }
-    });
-
-    return winner;
-  };
-
-  Game.prototype.horizontalWinner = function () {
-    var game = this;
-		var horizontalPositions1 = [0, 1, 2];
-		var horizontalPositions2 = [3, 4, 5];
-		var horizontalPositions3 = [6, 7, 8];
-
-    var winner = null;
-    _(Game.marks).each(function (mark) {
-      function didWinVertical (horizontalPositions) {
-        return _.every(horizontalPositions, function (pos) {
-          return game.board[pos] === mark;
-				});
-			}
-
-      var won = _.any(
-        [horizontalPositions1, horizontalPositions2, horizontalPositions3],
-        didWinVertical
-      );
-
-      if (won) {
-        winner = mark;
-      }
-    });
-
-    return winner;
-  };
-  
-  Game.prototype.winner = function () {
-    return (
-      this.diagonalWinner() || this.horizontalWinner() || this.verticalWinner()
-    );
-  };
+  // Game.prototype.diagonalWinner = function () {
+  //   var game = this;
+  // 
+  //   var diagonalPositions1 = [0, 4, 8];
+  //   var diagonalPositions2 = [2, 4, 6];
+  // 
+  //   var winner = null;
+  //   _(Game.marks).each(function (mark) {
+  //       function didWinDiagonal (diagonalPositions) {
+  //       return _.every(diagonalPositions, function (pos) {
+  //         return game.board[pos] === mark;
+  //       });
+  //     }
+  // 
+  //     var won = _.any(
+  //       [diagonalPositions1, diagonalPositions2],
+  //       didWinDiagonal
+  //     );
+  // 
+  //     if (won) {
+  //       winner = mark;
+  //     }
+  //   });
+  // 
+  //   return winner;
+  // };
+  // 
+  // Game.prototype.verticalWinner = function () {
+  //   var game = this;
+  //     var verticalPositions1 = [0, 3, 6];
+  //     var verticalPositions2 = [1, 4, 7];
+  //     var verticalPositions3 = [2, 5, 8];
+  // 
+  //   var winner = null;
+  //   _(Game.marks).each(function (mark) {
+  //     function didWinVertical (verticalPositions) {
+  //       return _.every(verticalPositions, function (pos) {
+  //         return game.board[pos] === mark;
+  //         });
+  //       }
+  // 
+  //     var won = _.any(
+  //       [verticalPositions1, verticalPositions2, verticalPositions3],
+  //       didWinVertical
+  //     );
+  // 
+  //     if (won) {
+  //       winner = mark;
+  //     }
+  //   });
+  // 
+  //   return winner;
+  // };
+  // 
+  // Game.prototype.horizontalWinner = function () {
+  //   var game = this;
+  //     var horizontalPositions1 = [0, 1, 2];
+  //     var horizontalPositions2 = [3, 4, 5];
+  //     var horizontalPositions3 = [6, 7, 8];
+  // 
+  //   var winner = null;
+  //   _(Game.marks).each(function (mark) {
+  //     function didWinVertical (horizontalPositions) {
+  //       return _.every(horizontalPositions, function (pos) {
+  //         return game.board[pos] === mark;
+  //         });
+  //       }
+  // 
+  //     var won = _.any(
+  //       [horizontalPositions1, horizontalPositions2, horizontalPositions3],
+  //       didWinVertical
+  //     );
+  // 
+  //     if (won) {
+  //       winner = mark;
+  //     }
+  //   });
+  // 
+  //   return winner;
+  // };
+  // 
+  // Game.prototype.winner = function () {
+  //   return (
+  //     this.diagonalWinner() || this.horizontalWinner() || this.verticalWinner()
+  //   );
+  // };
 
  
 
