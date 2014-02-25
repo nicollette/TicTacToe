8e86fb01c377f86347065b42f4a4791fa9eea12e
Nicollette <nicolletteml@gmail.com>
2014-02-25 12:26:44 -0800

    Change style

--- ttt.js
+++ ttt.js
@@ -10,33 +10,45 @@
     this.turn = "orange";
   };
 
-  Game.prototype.run = function () {
-    this.bindClicks();
-  };
-  
-	Game.prototype.placeMark = function(tile_id) {
-		this.board.tiles[parseInt(tile_id)] = this.turn;
-    this.switchPlayer();
-	};
-
-  Game.prototype.switchPlayer = function () {
-    if (this.turn === this.board.marks[0]) {
-      this.turn = this.board.marks[1];
-      $("#curr-player").text("Hal's turn");
-    } else {
-      this.turn = this.board.marks[0];
-      $("#curr-player").text("your turn");
-    }
-  };
-
 	Game.prototype.bindClicks = function() {
 		$('.tile').on('click', this.handleTClick.bind(this));
 	};
 
+	Game.prototype.changeTileColor = function($tile) {
+		if(this.turn === "blue") {
+			$tile.addClass('xtile');
+		}
+		else {
+  		$tile.addClass('otile');
+    }
+		$tile.off("click");
+	};
+
+	Game.prototype.gameOverClicks = function() {
+		$('.tile').off("click");
+	}
+
+  Game.prototype.handleComputerMove = function ($tile) {
+    this.changeTileColor($tile);
+    this.placeMark($tile[0].id);
+  };
+  
+  Game.prototype.handleGameOver = function () {
+    if (this.board.winner()) {
+      var winner = this.board.winner() == "orange" ? "you" : "Hal"
+      if(winner === "Hal") {
+        $('body').addClass('gameover');
+      }
+      $('#instructions').text(winner + ' won again')
+    } else if (this.board.isBoardFull()) {
+      $('#instructions').text('game over, no one wins');
+    }
+    this.gameOverClicks();
+  };
+
 	Game.prototype.handleTClick = function(event) {
     event.preventDefault();
-		this.changeTileColor(event.currentTarget);
-		$(event.currentTarget).off("click");
+		this.changeTileColor($(event.currentTarget));
 		this.placeMark(event.currentTarget.id);
     
     if (this.board.isGameOver()) {
@@ -56,40 +68,19 @@
     }
   };
   
-  Game.prototype.handleGameOver = function () {
-    if (this.board.winner()) {
-      var winner = this.board.winner() == "orange" ? "you" : "Hal"
-      alert(winner + " won!");
-    } else if (this.board.isBoardFull()) {
-      alert("Game over, no one wins.");
-    }
-    this.gameOverClicks();
-  };
-  
-  Game.prototype.handleComputerMove = function ($tile) {
-		if(this.turn === "blue") {
-			$tile.addClass('xtile');
-		}
-		else {
-			$tile.addClass('otile');
-		}
-    $tile.off("click");
-    this.placeMark($tile[0].id);
-  };
-  
-	Game.prototype.gameOverClicks = function() {
-		$('.tile').off("click");
-	}
-
-	Game.prototype.changeTileColor = function(tile) {
-		if(this.turn === "blue") {
-			$(tile).addClass('xtile');
-		}
-		else {
-			$(tile).addClass('otile');
-		}
+	Game.prototype.placeMark = function(tile_id) {
+		this.board.tiles[parseInt(tile_id)] = this.turn;
+    this.switchPlayer();
 	};
 
+  Game.prototype.run = function () {
+    this.bindClicks();
+  };
+
+  Game.prototype.switchPlayer = function () {
+    this.turn = this.turn === this.board.marks[0] ? 
+      this.board.marks[1] : this.board.marks[0];
+  };
 })(this);
 
 $(function () {
